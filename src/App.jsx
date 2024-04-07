f9c5661fbb8473f66c94ffa91a1a1710c0a14d70
Nelson <ngnelson8@gmail.com>
2024-04-07 15:11:36 +0100

    adding files

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import React from 'react'
+import { Navbar } from './components/Navbar'
+import { HeroSection } from './components/HeroSection'
+import { FeatureSection } from './components/FeatureSection'
+import { Workflow } from './components/Workflow'
+import { Pricing } from './components/Pricing'
+import { Testimonials } from './components/Testimonials'
+import { LoginSignUp } from './components/LoginSignUp'
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+      <HeroSection />
+      <FeatureSection />
+      <Workflow />
+      <Pricing />
+      <Testimonials />
+      </div>
+    </>
+  )
+}
